b24f101c9c85c384bcee1163355620ba236fa137
Vinicius Batista Barbosa <vinicius.batista.barbosa34@gmail.com>
2020-10-12 16:22:32 -0300

    Estrutura criada e typeorm configurado :fire:

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
 import express from 'express';
+import './Infrastructure/Database';
 
 const app = express();
+
+app.use(express.json());
+
+app.listen(3333, () => {
+	console.log('Server iniciado na porta 3333!');
+});
